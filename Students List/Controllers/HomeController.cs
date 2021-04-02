800cdde6f0bae3e4ca39d5c7dbe16ef9bcaf5ae5
mhsa-prn <piran_mahsa@yahoo.com>
2021-04-02 12:40:49 +0430

    Delete part was done

---
+++ Students List/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Students_List.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Students_List.Controllers
+{
+    public class HomeController : Controller
+
+    {
+        StudentRepository repository = new StudentRepository();
+        List<Student> model;
+        public int idd;
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            model = repository.select();
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+
+            repository.insert(student);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Delete(Student student)
+        {
+
+            repository.delete(student);
+            //return View();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(Student student)
+        {
+
+            repository.setIdd(student.id);
+
+            return View(student);
+        }
+
+        public IActionResult Update2(Student student)
+        {
+
+            repository.update(student);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Students List/Models/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Students_List.Models
+{
+    public class Student
+    {
+        
+        public int id { set; get; }
+        public string name { set; get; }
+    }
+
+    public class StudentRepository
+    {
+        public static List<Student> students_tbl = new List<Student>();
+        public int idd=0;
+
+        public List<Student> select()
+        {
+            return students_tbl;
+        }
+        public void insert(Student student)
+        {
+            students_tbl.Add(student);
+        }
+
+        public void delete(Student student)
+        {
+            int index = -1;
+            foreach (var x in students_tbl)
+            {
+                ++index;
+                if (x.id == student.id)
+                    break;
+            }
+            students_tbl.RemoveAt(index);
+        }
+
+        public void setIdd(int iddd)
+        {
+            idd = iddd;
+        }
+        public void update(Student student)
+        {
+            int index = -1;
+            foreach (var x in students_tbl)
+            {
+                ++index;
+                if (x.id == idd)
+                {
+                    x.id = student.id;
+                    x.name = student.name;
+                }
+            }
+            
+        }
+
+        
+    }
+}
